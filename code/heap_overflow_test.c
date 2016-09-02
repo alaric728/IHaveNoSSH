8200b3fc4ff0c6b8e66e4bbc8449e86fc468dde4
Christian Kuntz <christian.kuntz@uconn.edu>
2016-09-01 21:00:24 -0400

    Because Ubuntu has no SSH Server By Default

---
+++ code/heap_overflow_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUFSIZE 16
+#define OVERSIZE 8 /* Overflow of buf2 by OVERSIZE bytes */
+
+int main()
+{
+	u_long diff;
+	char *buf1 = (char *)malloc(BUFSIZE), *buf2 = (char *)malloc(BUFSIZE);
+	diff = (u_long)buf2 - (u_long)buf1;
+	printf("buf1 = %p, buf2 = %p, diff = 0x%x bytes\n", buf1, buf2, diff);
+
+	memset(buf2, 'A', BUFSIZE-1), buf2[BUFSIZE-1]= '\0';
+
+	printf("before overflow: buf2 = %s\n", buf2);
+	memset(buf1, 'B', (u_int)(diff + OVERSIZE));
+	printf("after overflow: buf2 = %s\n", buf2);
+
+	return 0;
+}
